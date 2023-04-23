eda63a2094d08f40a2692e92dded678adce63dde
AM_Erizur <50849821+Erizur@users.noreply.github.com>
2023-04-22 22:27:29 -0500

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,14 +1,17 @@
 // ==UserScript==
 // @name         Switch Hitchhiker to dark theme
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.youtube.com/
+// @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @resource style https://raw.githubusercontent.com/Erizur/rehike-darktheme/main/css/main.css
+// @downloadURL  https://github.com/Erizur/rehike-darktheme/raw/main/main.user.js
+// @updateURL    https://github.com/Erizur/rehike-darktheme/raw/main/main.user.js
 // @grant        GM_addStyle
-// @run-at       document-end
+// @grant        GM_getResourceText
+// @run-at       document-start
 // ==/UserScript==
 
 var darkStyle;
@@ -19,6 +22,7 @@ function checkTime()
     var darkTime = date.getHours() >= 19 ? true : false;
 
     if(darkTime && darkStyle == undefined) darkStyle = GM_addStyle(GM_getResourceText('style'));
+    if(darkTime == false && darkStyle !== undefined) darkStyle.remove();
 }
 
 function start_refresh(){
